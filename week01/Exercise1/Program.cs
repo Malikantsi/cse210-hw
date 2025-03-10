ba55a4e3fea26a15b9b70cdb4cabf9796856eb9d
malikantsi <lekhoanalucy@gmail.com>
2025-03-10 16:53:49 +0200

    done exercise 1 and 2

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("what is your name?");
+        string name = Console.ReadLine();
+
+        Console.WriteLine();
+        Console.Write("What is your last name?");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_name},{name} {last_name} ");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade :");
+        string userInputValue = Console.ReadLine();
+
+        int inputPercentage = int.Parse(userInputValue);
+        string letter = "";
+
+        if (inputPercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (inputPercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (inputPercentage >= 70)
+        {
+            letter = "C"; 
+        }
+        else if (inputPercentage >= 60)
+        {
+            letter = "D";
+        }
+        else if (inputPercentage < 60)
+        {
+            letter = "F";
+        }
+        else
+        {
+            Console.WriteLine("You did not enter the number in range");
+        }
+        Console.WriteLine($"You got a {letter}");
+
+        if (inputPercentage >= 70)
+        {
+            Console.WriteLine("Congradulations, You passed the class");
+        }
+        else
+        {
+            Console.WriteLine("Sorry you didnt make it,Try harder next time!");
+        }
+
     }
 }
